554799e1ea4aa65a9941ce7e9a9b7853efcaf389
Karen Arenas <karenas_31@hotmail.com>
2018-11-15 18:32:26 -0600

    Mod index fail

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ http.createServer(function (request, response) {
    // Send the HTTP header 
    // HTTP Status: 200 : OK
    // Content Type: text/plain
-   response.writeHead(200, {'Content-Type': 'text/plain'});
+   response.writeHead(200, {'Content-Type': 'text/plain'})
    
    // Send the response body as "Hello Karen"
    response.end('Hello World\n');
